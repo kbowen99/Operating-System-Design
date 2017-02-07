886ba80560c65ffc35afa3c5a6cd0f8839522d26
kbowen99 <kurtisbowen99@gmail.com>
2017-02-07 14:16:25 -0600

    I have No Idea...

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+
+
 // Put your code here.
+@RAMS //CurrentRams
+(MAGIC)//LABEL 
+
+@RAMS //Increment RAMS
+M=M+1
+
+@RAMS
+D=M
+@100
+D=D-A
+@END
+D;JGT
+
+@MAGIC 
+0;JMP
+(END)
+@END
+0;JMP
+
+
+
+
+
