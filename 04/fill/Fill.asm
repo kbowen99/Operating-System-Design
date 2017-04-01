8d5df2549a86fa3f274473a8a1772e030be1cf09
kbowen99 <kurtisbowen99@gmail.com>
2017-04-01 17:04:00 -0500

    04 Complete

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,11 +1,35 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(DOBLACK)
+	@24576	//infallible keyboard Register
+	D=M
+@DOWHITE	//Whiten if not pressed
+D;JEQ
+	@24575 //Keyboard - 1
+	D=M
+@DOWHITE //If White
+D;JLT
+	@CURRPIX
+	D=M
+	@16384	//infallible Screen
+	D=A+D
+	A=D
+	M=-1
+	@CURRPIX
+	M=M+1
+@DOBLACK
+0;JMP//All Loops are infinite... (if pressed)
 
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
-
-// Put your code here.
+(DOWHITE)
+	@24576	//Handy Dandy Keyboard
+	D=M
+@DOBLACK	//if pressed, Do Black
+D;JGT
+	@CURRPIX
+	D=M
+	@16384	//Screen
+	D=A+D
+	A=D
+	M=0
+	@CURRPIX
+	M=M-1	//change current pixel
+@DOWHITE
+0;JMP
